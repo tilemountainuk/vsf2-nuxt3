f1cc0bc5962e4b2848217e4e41665bff29bab7ee
fawadaslam <fawad@ki5.co.uk>
2023-08-11 19:34:00 +0500

    updated notification file

--- apps/web/components/ui/Notification.vue
+++ apps/web/components/ui/Notification.vue
@@ -3,17 +3,32 @@
     class="fixed md:max-w-lg w-auto top-14 mt-2 md:top-20 md:right-6 lg:right-10 inset-x-2 md:left-auto font-normal"
     data-testid="notifications"
   >
-    <div class="flex flex-col gap-2 items-end">
+    <div class="flex flex-col gap-2 items-end" v-if="isOpen && notifications.length > 0">
       <div
         role="alert"
-        v-if="isOpen && notifications.length > 0"
-        class="md:items-center max-w-[600px] shadow-md bg-positive-100 pr-2 pl-4 ring-1 ring-positive-200 typography-text-sm md:typography-text-base py-1 rounded-md"
+        v-for="(notification, index) in notifications"
+        :key="index"
+        :class="'bg-' + notification.type + '-100 bg-' + notification.type + '-100 ring-' + notification.type + '-200'"
+        class="md:items-center max-w-[600px] shadow-md pr-2 pl-4 ring-1 typography-text-sm md:typography-text-base py-1 rounded-md"
       >
-        <div v-for="(notification, index) in notifications" :key="index" class="flex items-start">
+        <div class="flex items-start" :class="notification.type">
           <p class="py-2 mr-2">{{ notification.message }}</p>
           <button
             type="button"
-            class="p-1.5 md:p-2 ml-auto rounded-md text-positive-700 hover:bg-positive-200 active:bg-positive-300 hover:text-positive-800 active:text-positive-900 focus-visible:outline focus-visible:outline-offset"
+            :class="
+              'text-' +
+              notification.type +
+              '-700 hover:bg-' +
+              notification.type +
+              '-200 hover:text-' +
+              notification.type +
+              '-800 active:bg-' +
+              notification.type +
+              '-300 active:text-' +
+              notification.type +
+              '-900'
+            "
+            class="p-1.5 md:p-2 ml-auto rounded-md focus-visible:outline focus-visible:outline-offset"
             aria-label="Close positive alert"
             @click="close"
           >
