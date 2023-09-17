b3c07ddfd25f011dbcd99035016e8f873783a549
Syed Haider Hassan <syed.haider@ki5.co.uk>
2023-09-17 23:25:12 +0500

    just testing if everything working

--- apps/server/middleware.config.ts
+++ apps/server/middleware.config.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const cookieNames = {
@@ -17,40 +18,47 @@ const config = {
     magento: {
       location: '@vue-storefront/magento-api/server',
       customQueries: {
-        'category-search-custom-query': ({ variables, metadata }: {variables:any, metadata: any}) => ({
+        'products-my-custom-query': ({ variables, metadata }: { variables: any, metadata: any }) => ({
           variables,
           query: `
-          query categorySearch($filters: CategoryFilterInput) {
-            categoryList(filters: $filters) {
-              ${metadata.fields}
-            }
-          }`
-        }),
+             query productsList(
+               $search: String = "",
+               $filter: ProductAttributeFilterInput,
+               $pageSize: Int = 10,
+               $currentPage: Int = 1
+               $sort: ProductAttributeSortInput
+             ) {
+               products(search: $search, filter: $filter, sort: $sort, pageSize: $pageSize, currentPage: $currentPage) {
+                 ${metadata.fields}
+               }
+             }
+           `
+        })
       },
       configuration: {
         api: process.env.VSF_MAGENTO_GRAPHQL_URL,
         cookies: {
-          ...cookieNames,
+          ...cookieNames
         },
         cookiesDefaultOpts: {
           httpOnly: process.env.VSF_COOKIE_HTTP_ONLY || false,
           secure: process.env.VSF_COOKIE_SECURE || false,
           sameSite: process.env.VSF_COOKIE_SAME_SITE || 'lax',
-          path: process.env.VSF_COOKIE_PATH || '/',
+          path: process.env.VSF_COOKIE_PATH || '/'
         },
         defaultStore: 'default',
         customApolloHttpLinkOptions: {
-          useGETForQueries: true,
+          useGETForQueries: true
         },
         magentoBaseUrl: process.env.VSF_MAGENTO_BASE_URL,
         magentoApiEndpoint: process.env.VSF_MAGENTO_GRAPHQL_URL,
         imageProvider: process.env.NUXT_IMAGE_PROVIDER,
         customer: {
-          customer_create_account_confirm: true,
-        },
-      },
+          customer_create_account_confirm: true
+        }
+      }
     }
-  },
+  }
 };
 
 export default config;
---
+++ apps/web/pages/something.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div v-if="products">
+      <h1>Product List</h1>
+      <ul>
+        <li v-for="product in products.items" :key="product.id">
+          {{ product.name }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      No products found.
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { sdk } from '~/sdk';
+const { magento } = sdk;
+
+interface Product {
+  id: string;
+  name: string;
+  sku: string;
+
+  [key: string]: string;
+  // ... other fields
+}
+
+interface ProductsResponse {
+  items: Product[];
+  // ... other fields
+}
+
+const products = ref<ProductsResponse | null>(null);
+
+async function fetchProducts() {
+  try {
+
+    const customQuery = {
+      products: 'products-my-custom-query',
+      metadata: {
+        fields: 'items { id sku name }',
+      },
+    };
+
+    const { data } = await magento.products({
+      filter: {
+        sku: {
+          eq: '445520', // optional SKU filter
+        },
+      },
+    }, { customQuery });
+
+    if (data) {
+      products.value = data.products as unknown as ProductsResponse;  // Typecast if necessary
+    }
+  } catch (error) {
+    console.error("Failed to fetch products:", error);
+  }
+}
+
+// Fetch products on component mount
+fetchProducts();
+</script>
+
