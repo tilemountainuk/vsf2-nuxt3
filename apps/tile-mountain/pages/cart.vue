33f845e21c647db10cebcecc462037eb672786cd
fawadaslam <fawad@ki5.co.uk>
2023-09-22 18:42:13 +0500

    feat: Final theme concept

--- apps/tile-mountain/pages/cart.vue
+++ apps/tile-mountain/pages/cart.vue
@@ -6,20 +6,13 @@
       :back-label-mobile="$t('back')"
       :heading="$t('myCart')"
     >
-        this is child cart
+      Tile-Mountain Cart Page
       <CartPageContent />
     </NuxtLayout>
 </template>
-  
 <script setup lang="ts">
-import { useSdk } from '../../web/sdk';
-    definePageMeta({
-        layout: false,
-    });
-    onMounted(async () => {
-        console.log('This is called on mount of child');
-        const { data, error } = await useAsyncData(() => useSdk().magento.createEmptyCart());
-        console.log('This is data', data);
-    });
+definePageMeta({
+layout: false,
+});
 </script>
   
--- apps/tile-mountain/pages/index.vue
+++ apps/tile-mountain/pages/index.vue
@@ -1,4 +1,4 @@
 <template>
-    <div>This is home page from child theme</div>
+    <div>This is home page from tile-mountain theme</div>
 </template>
 <script lang="ts"></script>
