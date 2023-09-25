868a569fc79316e875f0c1a91f04534ddee1cade
Syed Haider Hassan <syed.haider@ki5.co.uk>
2023-09-25 11:43:10 +0500

    we have themes directory now

--- apps/web/sdk/index.ts
+++ apps/web/sdk/index.ts
@@ -1,3 +1,4 @@
+import { magentoModule, MagentoModuleType } from '@vue-storefront/magento-sdk';
 import { initSDK, buildModule } from '@vue-storefront/sdk';
 import { type SdkModule, sdkModule } from '@vue-storefront/storefront-boilerplate-sdk';
 import { createSharedComposable } from '@vueuse/core';
@@ -5,6 +6,9 @@ import { createSharedComposable } from '@vueuse/core';
 export const useSdk = createSharedComposable(() => {
   const sdkConfig = {
     commerce: buildModule<SdkModule>(sdkModule),
+    magento: buildModule<MagentoModuleType>(magentoModule, {
+      apiUrl: 'http://localhost:4000/magento',
+    }),
   };
 
   return initSDK<typeof sdkConfig>(sdkConfig);
---
+++ apps/web/themes/tiles247/modules/product/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  Product Page here for SKU {{ sku }}
+</template>
+
+<script lang="ts" setup>
+import { ref, defineProps } from 'vue';
+
+const props = defineProps<{
+  sku: string;
+}>();
+
+const sku = ref(props.sku);
+</script>
