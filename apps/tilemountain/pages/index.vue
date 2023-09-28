602967332dea36b585c31903085d6a2834f9d016
Syed Haider Hassan <syed.haider@ki5.co.uk>
2023-09-29 00:51:46 +0500

    CMS BLocks Fetching

--- apps/tilemountain/pages/index.vue
+++ apps/tilemountain/pages/index.vue
@@ -1,12 +1,39 @@
 <template>
-  <div v-for="(component, index) in content" :key="`${component.fields.component}-${index}`">
-    <RenderContent :content="component.fields.content" />
+  is this mobile Page? :: {{ isMobile }}
+  <pre>
+  {{ data }}
+  </pre>
+  <div v-if="data && data?.cmsBlocks?.items">
+    <template v-for="cmsBlock in data.cmsBlocks.items">
+      <div v-if="cmsBlock" v-html="cmsBlock.content" />
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
-const { getContent, data: content } = useContent('home-page');
+// const { getContent, data: content } = useContent('home-page');
+const { getCmsBlocks, data } = useCmsBlocks({ page: 'home' });
 const { isMobile } = useDevice()
 
-getContent();
+//
+// userAgent: string
+// isDesktop: boolean
+// isIos: boolean
+// isAndroid: boolean
+// isMobile: boolean
+// isMobileOrTablet: boolean
+// isDesktopOrTablet: boolean
+// isTablet: boolean
+// isWindows: boolean
+// isMacOS: boolean
+// isApple: boolean
+// isSafari: boolean
+// isFirefox: boolean
+// isEdge: boolean
+// isChrome: boolean
+// isSamsung: boolean
+// isCrawler: boolean
+
+
+getCmsBlocks();
 </script>
