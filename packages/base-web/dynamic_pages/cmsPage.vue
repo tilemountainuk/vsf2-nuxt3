f91054e23808a8b892804081cc741fbe9bc82013
Syed Haider Hassan <syed.haider@ki5.co.uk>
2023-10-01 23:35:10 +0500

    Architectural Changes. Working on CMS, Have the CMS blocks, need to work on module to fetch and have CMS page populated as well.
    Along with that reworked the [...slug].vue as parent child concept was not working with previous approach.

--- packages/base-web/dynamic_pages/cmsPage.vue
+++ packages/base-web/dynamic_pages/cmsPage.vue
@@ -1,3 +1,19 @@
 <template>
-  its a CMS Page (base)
+  <h1 class="text-2xl lg:text-6xl font-bold text-center text-gray-300 bg-gray-900 p-5 rounded-lg shadow-lg">
+    <span v-if="cmsPage?.identifier">identifier :: {{ cmsPage.identifier }}</span>
+    <span class="block text-center text-xl text-gray-500 bg-gray-800 px-3 py-1 rounded-full mt-2 inline-block">
+      Tailwind Works
+    </span>
+  </h1>
+  <p v-if="cmsPage?.content" v-html="cmsPage.content" />
 </template>
+<script lang="ts" setup>
+import { useCmsPage } from '@base/modules/cms/runtime/composables/useCmsPage';
+
+const { identifier } = defineProps<{
+  identifier: string;
+}>();
+
+const { getCmsPage, loading } = useCmsPage()
+const cmsPage = await getCmsPage(identifier)
+</script>
