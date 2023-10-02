511272f958a1227da77aa5853340508707721e18
fawadaslam1408 <fawad@ki5.co.uk>
2023-10-02 15:23:52 +0500

    Header Top Bar desktop

--- apps/web/components/HeaderIcon/AccountIcon.vue
+++ apps/web/components/HeaderIcon/AccountIcon.vue
@@ -6,7 +6,7 @@
     xmlns:xlink="http://www.w3.org/1999/xlink"
     x="0px"
     y="0px"
-    width="20px"
+    width="23px"
     viewBox="0 0 450.4 450.4"
     xml:space="preserve"
   >
--- apps/web/components/ui/NavbarTop.vue
+++ apps/web/components/ui/NavbarTop.vue
@@ -1,10 +1,10 @@
 <template>
   <header>
     <div
-      class="px15 box-border ml-auto mr-auto pr-2 pl-2 max-w-full md:w-[752px] 2md:w-[976px] xl:w-[1280px] mp:pr-0 mp:pl-0 pl-[15px] pr-[15px]"
+      class="px15 box-border ml-auto mr-auto pr-2 pl-2 max-w-full md:w-[752px] xl:w-[1280px] 2md:w-[976px] mp:pr-0 mp:pl-0 pl-[15px] pr-[15px]"
     >
       <div
-        class="row between-xs middle-xs header-main box-border flex flex-initial flex-row flex-wrap -mr-2 -ml-2 justify-between items-center ml-2 tab:-ml-[6px] mp:m-0"
+        class="between-xs middle-xs header-main box-border flex flex-initial flex-row flex-wrap -mr-2 -ml-2 justify-between items-center ml-2 tab:-ml-[6px] mp:m-0"
       >
         <div
           class="col-md-2 col-xs-4 md:box-border md:pr-2 md:pl-2 md:flex-[0_0_auto] md:basis-2/12 md:max-w-[16.66666667%] basis-2/6 max-w-[33.33333333%]"
@@ -51,7 +51,7 @@
           </div>
         </div>
         <div
-          class="md:box-border md:pr-2 md:pl-2 md:flex-[0_0_auto] md:basis-4/12 md:max-w-[33.33.333337%] 2md:box-border 2md:pr-0 2md:pl-0 2md:flex-[0_0_auto] 2md:basis-5/12 2md:max-w-[41.666667%] basis-8/12 max-w-[66.666667%] relative pr-[34px] pr-[34px] text-right 1tab:pr-[50px] mp:pr-[6px] 4xs:pr-0 mp:h-full mp:flex mp:max-h-[50px] mp:justify-end mp:items-center mp:pt-0"
+          class="md:box-border md:pr-2 md:pl-2 md:flex-[0_0_auto] md:basis-4/12 md:max-w-[33.333337%] 2md:box-border 2md:pr-0 2md:pl-0 2md:flex-[0_0_auto] 2md:basis-5/12 2md:max-w-[41.666667%] basis-8/12 max-w-[66.666667%] relative pr-[34px] pr-[34px] text-right 1tab:pr-[50px] mp:pr-[6px] 4xs:pr-0 mp:h-full mp:flex mp:max-h-[50px] mp:justify-end mp:items-center mp:pt-0"
         >
           <div class="inline-flex mp:flex mp:justify-end mp:pr-[36px] 4xs:pr-[32px]">
             <div class="flex pl-[55px] 1tab:pl-[10px] tab:pl-0">
@@ -60,10 +60,10 @@
               >
                 <NuxtLink
                   to="/i/contact-us"
-                  class="flex pt-[8px] pt-[8px] whitespace-nowrap bg-[url('https://www.tilemountain.co.uk/assets/phone.svg')] bg-no-repeat bg-[length:26px] 1tab:bg-[length:24px] tab:bg-[length:16px] mp:bg-[length:24px] 4xs:bg-[length:20px] 1tab:pt-[20px] 1tab:pr-0 1tab:pb-[13px] 1tab:pl-[13px] 1tab:bg-[length:24px] mp:flex mp:whitespace-nowrap mp:h-[72px] mp:w-full mp:max-w-[35px] mp:p-0"
+                  class="flex pt-[8px] pt-[8px] whitespace-nowrap bg-[url('https://www.tilemountain.co.uk/assets/phone.svg')] bg-left bg-center bg-[60%] bg-no-repeat bg-[length:26px] 1tab:bg-[length:24px] tab:bg-[length:16px] mp:bg-[length:24px] 4xs:bg-[length:20px] 1tab:pt-[20px] 1tab:pr-0 1tab:pb-[13px] 1tab:pl-[13px] 1tab:bg-[length:24px] mp:flex mp:whitespace-nowrap mp:h-[72px] mp:w-full mp:max-w-[35px] mp:p-0"
                 >
                   <p
-                    class="text-[#29275b] font-bold ml-[2px] ml-[2px] text-[18px] pl-[29px] mp:hidden 1tab:text-[14px] 1tab:pl-[22px] tab:text-[12px] tab:pl-[12px]"
+                    class="mt-[18px] mr-0 mb-[18px] ml-[2px] text-[#29275b] font-bold ml-[2px] ml-[2px] text-[18px] pl-[29px] mp:hidden 1tab:text-[14px] 1tab:pl-[22px] tab:text-[12px] tab:pl-[12px]"
                   >
                     Need help?
                   </p>
@@ -73,19 +73,17 @@
                 <div
                   class="bg-transparent border-0 cursor-pointer pt-[12px] pr-[34px] pb-[12px] pl-[12px] mp:p-0 1tab:pt-[8px] 1tab:pr-[16px] 1tab:pb-[12px] 1tab:pl-[12px] tab:p-[6px] mp:w-[14vw]"
                 >
-                  <button type="button" class="bg-transparent border-0 p-0 location">
-                    <NuxtLink to="/i/contact-us">
-                      <HeaderShowroomIcon />
-                      <p class="m-0 md:pt-[6px] hidden md:text-[#29275b] md:block">Showrooms</p>
-                    </NuxtLink>
-                  </button>
+                  <NuxtLink to="/i/contact-us" class="p-0">
+                    <HeaderShowroomIcon class="mt-[25px] mb-auto mx-auto" />
+                    <p class="text-[13.33px] m-0 md:pt-[6px] hidden md:text-[#29275b] md:block">Showrooms</p>
+                  </NuxtLink>
                 </div>
                 <div
                   class="cursor-pointer pt-[12px] pr-[36px] pb-[12px] pl-[12px] 1tab:pt-[8px] 1tab:pr-[16px] 1tab:pb-[12px] 1tab:pl-[12px] tab:pt-[8px] tab:pr-[12px] tab:pb-[12px] tab:pl-[12px] ph:text-right ph:mr-[2px] ph:p-0"
                 >
                   <div class="inline-flex relative pr-[13px] outline-0 tab:pr-[6px] ph:p0">
-                    <NuxtLink to="#">
-                      <HeaderAccountIcon />
+                    <NuxtLink to="#" class="pt-[16px]">
+                      <HeaderAccountIcon class="mt-[8px]" />
                     </NuxtLink>
                   </div>
                 </div>
@@ -94,8 +92,12 @@
                 >
                   <HeaderCartIcon />
                   <span
-                    class="top-[32px] left-[15px] w-[18px] h-[18px] rounded-[50%] flex justify-center items-center bg-[#ed108c] 1tab:top-[28px] 1tab:left-[12px] 1tab:w-[16px] 1tab:h-[16px] 1tab:text-[10px] tab:top-[27px] tab:left-[9px] tab:w-[14px] tab:h-[14px] tab:text-[9px] mp:top-[20px] mp:left-[20px] mp:w-[22px] mp:h-[22px] mp:text-[14px] 4xs:top-[24px] 4xs:left-[12px] 4xs:w-[16px] 4xs:h-[16px] 4xs:text-[10px] absolute flex justify-center text-center items-center mp:mt-0 mp:mb-0 mp:-mr-[15px] mp:-ml-[15px] border-box py0 px2 h6 lh16 weight-700 cl-white"
+                    class="text-white top-[32px] left-[15px] w-[18px] h-[18px] rounded-[50%] flex justify-center items-center bg-[#ed108c] 1tab:top-[28px] 1tab:left-[12px] 1tab:w-[16px] 1tab:h-[16px] 1tab:text-[10px] tab:top-[27px] tab:left-[9px] tab:w-[14px] tab:h-[14px] tab:text-[9px] mp:top-[20px] mp:left-[20px] mp:w-[22px] mp:h-[22px] mp:text-[14px] 4xs:top-[24px] 4xs:left-[12px] 4xs:w-[16px] 4xs:h-[16px] 4xs:text-[10px] absolute flex justify-center text-center items-center mp:mt-0 mp:mb-0 mp:-mr-[15px] mp:-ml-[15px] border-box py0 px2 h6 lh16 weight-700 cl-white"
                     >0</span
+                  >
+                  <span
+                    class="text-lg font-bold text-[#29275b] ml-[15px] mt-[17px] ph:text-sm ph:ml-2.5 ph:mt-1 tab:hidden"
+                    >£0.00</span
                   >
                 </button>
               </div>
@@ -126,3 +128,4 @@ defineProps<{
   filled?: boolean;
 }>();
 </script>
+<style></style>
