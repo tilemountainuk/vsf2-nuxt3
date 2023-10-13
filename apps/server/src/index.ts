292eefc8e35a8ba08fc7b372467775b0658df2e8
Syed Haider Hassan <syed.haider@ki5.co.uk>
2023-10-13 21:27:02 +0500

    updated server app to allow defined hosts.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,40 @@
 import express from 'express';
+import cors from 'cors'
 import { createServer } from '@vue-storefront/middleware';
 import '@vue-storefront/magento-api/server/index.js';
 import config from './middleware.config';
 // const app = express();
 (async () => {
   const app = await createServer({ integrations: config.integrations });
+
+  const allowedHosts = [
+    ".*divante.pl",
+    ".*vuestorefront.io",
+    ".*bathroom.com",
+    ".*tiles247.co.pk",
+    ".*tilemountain.co.uk",
+    ".*tm8.co.uk",
+    ".*vercel.app",
+    "localhost"
+  ];
+
+  app.use(cors({
+    origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+      if (!origin) return callback(null, true); // allow requests with no origin
+
+      const isAllowed = allowedHosts.some((host) => {
+        const regex = new RegExp(host, 'i'); // case-insensitive
+        return regex.test(origin);
+      });
+
+      if (isAllowed) {
+        return callback(null, true);
+      } else {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+    }
+  }));
 
   app.get('/', (req, res) => {
     res.send('Welcome to the simple Express server!');
