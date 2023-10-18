41f6b465dbc8d081ee48e3b5b8e056b9da376006
henakhaleeq <hena.khaleeq@ki5.co.uk>
2023-10-18 13:02:17 +0500

    working on category page header

--- apps/web/pages/category.vue
+++ apps/web/pages/category.vue
@@ -1,26 +1,20 @@
 <template>
-  <NuxtLayout name="default" :breadcrumbs="breadcrumbs">
-    <CategoryPageContent
-      v-if="productsCatalog"
-      :title="$t('allProducts')"
-      :total-products="productsCatalog.pagination.totalResults"
-      :products="productsCatalog.products"
-    >
-      <template #sidebar>
-        <CategoryTree :categories="categories" :parent="{ name: $t('allProducts'), href: paths.category }" />
-        <CategorySorting />
-        <CategoryFilters :facets="productsCatalog.facets" />
-      </template>
-    </CategoryPageContent>
-  </NuxtLayout>
+  <CategoryPageContent
+    v-if="productsCatalog"
+    :title="$t('allProducts')"
+    :total-products="productsCatalog.pagination.totalResults"
+    :products="productsCatalog.products"
+  >
+    <template #sidebar>
+      <CategoryTree :categories="categories" :parent="{ name: $t('allProducts'), href: paths.category }" />
+      <CategorySorting />
+      <CategoryFilters :facets="productsCatalog.facets" />
+    </template>
+  </CategoryPageContent>
 </template>
 
 <script setup lang="ts">
 import type { Breadcrumb } from '~/components/ui/Breadcrumbs/types';
-
-definePageMeta({
-  layout: false,
-});
 
 const { fetchProducts, data: productsCatalog } = useProducts();
 const { t } = useI18n();
