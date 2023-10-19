e78df4ac95adc889d8be71585f41162801addf3a
henakhaleeq <hena.khaleeq@ki5.co.uk>
2023-10-19 13:30:16 +0500

    working on category-page

--- apps/web/components/CategoryPageContent/CategoryPageContent.vue
+++ apps/web/components/CategoryPageContent/CategoryPageContent.vue
@@ -1,94 +1,92 @@
 <template>
-  <NarrowContainer>
-    <div class="mb-20 px-4 md:px-0" data-testid="category-layout">
-  <div class="hidden mp:flex justify-center">
-    isDevice: {{ isDevice }} 
-      <h1 class="  font-bold text-mobile-menu text-4xl leading-6 ">Floor Tiles</h1>
-  </div>
-  <div class="flex my-10 justify-between items-center">
- 
-  <div> 
-      <span class=" flex justify-between items-center gap-2">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-      <path d="M4 19C3.71667 19 3.47933 18.904 3.288 18.712C3.096 18.5207 3 18.2833 3 18C3 17.7167 3.096 17.4793 3.288 17.288C3.47933 17.096 3.71667 17 4 17H8C8.28333 17 8.521 17.096 8.713 17.288C8.90433 17.4793 9 17.7167 9 18C9 18.2833 8.90433 18.5207 8.713 18.712C8.521 18.904 8.28333 19 8 19H4ZM4 7C3.71667 7 3.47933 6.90433 3.288 6.713C3.096 6.521 3 6.28333 3 6C3 5.71667 3.096 5.479 3.288 5.287C3.47933 5.09567 3.71667 5 4 5H12C12.2833 5 12.521 5.09567 12.713 5.287C12.9043 5.479 13 5.71667 13 6C13 6.28333 12.9043 6.521 12.713 6.713C12.521 6.90433 12.2833 7 12 7H4ZM12 21C11.7167 21 11.4793 20.904 11.288 20.712C11.096 20.5207 11 20.2833 11 20V16C11 15.7167 11.096 15.479 11.288 15.287C11.4793 15.0957 11.7167 15 12 15C12.2833 15 12.521 15.0957 12.713 15.287C12.9043 15.479 13 15.7167 13 16V17H20C20.2833 17 20.5207 17.096 20.712 17.288C20.904 17.4793 21 17.7167 21 18C21 18.2833 20.904 18.5207 20.712 18.712C20.5207 18.904 20.2833 19 20 19H13V20C13 20.2833 12.9043 20.5207 12.713 20.712C12.521 20.904 12.2833 21 12 21ZM8 15C7.71667 15 7.479 14.904 7.287 14.712C7.09567 14.5207 7 14.2833 7 14V13H4C3.71667 13 3.47933 12.904 3.288 12.712C3.096 12.5207 3 12.2833 3 12C3 11.7167 3.096 11.479 3.288 11.287C3.47933 11.0957 3.71667 11 4 11H7V10C7 9.71667 7.09567 9.479 7.287 9.287C7.479 9.09567 7.71667 9 8 9C8.28333 9 8.521 9.09567 8.713 9.287C8.90433 9.479 9 9.71667 9 10V14C9 14.2833 8.90433 14.5207 8.713 14.712C8.521 14.904 8.28333 15 8 15ZM12 13C11.7167 13 11.4793 12.904 11.288 12.712C11.096 12.5207 11 12.2833 11 12C11 11.7167 11.096 11.479 11.288 11.287C11.4793 11.0957 11.7167 11 12 11H20C20.2833 11 20.5207 11.0957 20.712 11.287C20.904 11.479 21 11.7167 21 12C21 12.2833 20.904 12.5207 20.712 12.712C20.5207 12.904 20.2833 13 20 13H12ZM16 9C15.7167 9 15.4793 8.904 15.288 8.712C15.096 8.52067 15 8.28333 15 8V4C15 3.71667 15.096 3.479 15.288 3.287C15.4793 3.09567 15.7167 3 16 3C16.2833 3 16.5207 3.09567 16.712 3.287C16.904 3.479 17 3.71667 17 4V5H20C20.2833 5 20.5207 5.09567 20.712 5.287C20.904 5.479 21 5.71667 21 6C21 6.28333 20.904 6.521 20.712 6.713C20.5207 6.90433 20.2833 7 20 7H17V8C17 8.28333 16.904 8.52067 16.712 8.712C16.5207 8.904 16.2833 9 16 9Z" fill="#172554"/>
-      </svg>
-      <p class="font-bold text-mobile-menu text-lg tab:text-sm ">Refine by</p>
-      </span>
-  </div>
-  <div class="flex mp:hidden"> 
-      <h1 class="font-bold text-mobile-menu text-4xl tab:text-2xl leading-6 ">Floor Tiles</h1>
-  </div>
-  <div class="flex justify-between items-center gap-8 tab:gap-4 ">
-  <div ref="referenceRef" class="relative">
-    <div
-      :id="id"
-      ref="selectTriggerRef"
-      role="combobox"
-      :aria-controls="listboxId"
-      :aria-expanded="isOpen"
-      aria-label="Select one option"
-      :aria-activedescendant="selectedOption ? `${listboxId}-${selectedOption.value}` : undefined"
-      class="flex items-center gap-8 tab:gap-6 relative font-normal typography-text-base tab:text-sm ring-1 ring-neutral-300 ring-inset rounded-md min-w-[215px] tab:min-w-[161px]  py-2 px-4 tab:py-1.5 tab:px-3 hover:ring-primary-700 active:ring-primary-700 active:ring-2 focus:ring-primary-700 focus:ring-2 focus-visible:outline focus-visible:outline-offset cursor-pointer"
-      tabindex="0"
-      @keydown.space="toggle()"
-      @click="toggle()"
-    >
-      <template v-if="selectedOption">{{ selectedOption.label }}</template>
-      <SfIconExpandMore
-        class="ml-auto text-neutral-500 transition-transform ease-in-out duration-300"
-        :class="{ 'rotate-180': isOpen }"
-      />
+  <div class="px-4 md:px-0" data-testid="category-layout">
+    <div class="hidden mp:flex justify-center my-10 ">
+      <h1 class="font-bold  text-mobile-menu text-2xl">Floor Tiles</h1>
     </div>
-    <ul
-      v-show="isOpen"
-      :id="listboxId"
-      ref="floatingRef"
-      role="listbox"
-      class="w-full py-2 rounded-md shadow-md border border-neutral-100 bg-white z-10"
-      :style="dropdownStyle"
-    >
-      <SfListItem
-        v-for="option in options"
-        :id="`${listboxId}-${option.value}`"
-        :key="option.value"
-        role="option"
-        tabindex="0"
-        :aria-selected="option.value === selectedOption?.value"
-        class="block"
-        :class="{ 'font-medium': option.value === selectedOption?.value }"
-        @click="selectOption(option)"
-        @keydown.enter="selectOption(option)"
-        @keydown.space="selectOption(option)"
-      >
-        {{ option.label }}
-      </SfListItem>
-    </ul>
-  </div>
-  <div>
+  <div class="flex my-10 justify-between items-center gap-4"> 
+    <div> 
+      <span class="  flex justify-start items-center gap-2">
+        <SfButton @click="open" variant="tertiary" class=" mp:px-0 whitespace-nowrap">
+          <template #prefix>
+          <SfIconTune class="fill-mobile-menu"/>
+          </template>
+          <p class="font-bold mp:font-medium text-mobile-menu text-lg tab:text-sm mp:text-sm  ">FIlters</p>
+        </SfButton>
+      </span>
+    </div>
+    <div class="flex mp:hidden"> 
+      <h1 class="font-bold text-mobile-menu text-4xl tab:text-2xl leading-6 ">Floor Tiles</h1>
+    </div>
+    <div class="flex justify-end items-center gap-2 ">
+      <div ref="referenceRef" class="relative flex justify-start items-center gap-1                   ">
+        <div class=" hidden mp:flex whitespace-nowrap  text-mobile-menu text-sm font-medium">
+          Sort by:
+        </div>
+        <div
+          :id="id"
+          ref="selectTriggerRef"
+          role="combobox"
+          :aria-controls="listboxId"
+          :aria-expanded="IsDropdownOpen"
+          aria-label="Select one option"
+          :aria-activedescendant="selectedOption ? `${listboxId}-${selectedOption.value}` : undefined"
+          class="flex items-center justify-between gap-8 tab:gap-6 mp:gap-1 relative whitespace-nowrap font-normal mp:font-medium typography-text-base tab:text-sm mp:text-xs text-[#18181B80] mp:text-[#71717A] ring-1 ring-neutral-300 ring-inset rounded-md md:w-[215px] mp:w-[128px] mp:min-w-[128px] mp:h-[26px] py-2 px-4 tab:py-1.5 tab:px-3 mp:py-1.5 mp:px-1.5 hover:ring-primary-700 active:ring-primary-700 active:ring-2 focus:ring-primary-700 focus:ring-2 focus-visible:outline focus-visible:outline-offset cursor-pointer"
+          tabindex="0"
+          @keydown.space="RecomendedDropdownToggle()"
+          @click="RecomendedDropdownToggle()"
+        >
+          <template v-if="selectedOption">
+            <span>
+              {{ selectedOption.label }}
+            </span>
+          </template>
+          <div>
+            <SfIconExpandMore
+            class="ml-auto text-mobile-menu mp:text-[#71717A] transition-transform ease-in-out duration-300"
+            :class="{ 'rotate-180': IsDropdownOpen }"
+          />
+          </div>
+        
+        </div>
+        <ul
+          v-show=IsDropdownOpen
+          :id="listboxId"
+          ref="floatingRef"
+          role="listbox"
+          class=" md:w-[215px] mp:w-[128px] mp:min-w-[128px] mp:left-[54.6485px] py-2 rounded-md shadow-md border border-neutral-100 bg-white z-10"
+          :style="dropdownStyle"
+        >
+          <SfListItem
+            v-for="option in options"
+            :id="`${listboxId}-${option.value}`"
+            :key="option.value"
+            role="option"
+            tabindex="0"
+            :aria-selected="option.value === selectedOption?.value"
+            class="block whitespace-nowrap font-normal mp:font-medium typography-text-base tab:text-sm mp:text-xs text-[#18181B80] mp:text-[#71717A]"
+            :class="{ 'font-medium': option.value === selectedOption?.value }"
+            @click="selectOption(option)"
+            @keydown.enter="selectOption(option)"
+            @keydown.space="selectOption(option)"
+          >
+            {{ option.label }}
+          </SfListItem>
+        </ul>
+      </div>
+    <div>
     <span>
-      <p class="font-bold text-lg tab:text-sm text-mobile-menu"> 291 Results</p>
+      <p class=" whitespace-nowrap font-bold mp:font-medium  text-lg tab:text-sm mp:text-xs text-mobile-menu"> 80 Result</p>
     </span>
   </div>
   </div>
   </div>
   <div class="md:flex gap-6" data-testid="category-page-content">
-        <CategorySidebar :is-open="isOpen" @close="close">
+        <CategorySidebar :is-open="IsRefinebyfilteropen" @close="IsRefinebyfilterclose">
           <NuxtLazyHydrate when-visible>
             <slot name="sidebar" />
           </NuxtLazyHydrate>
         </CategorySidebar>
         <div class="flex-1">
-          <div class="flex justify-between items-center mb-6">
-            <span class="font-bold font-headings md:text-lg">
-              {{ $t('numberOfProducts', { count: totalProducts }) }}
-            </span>
-            <SfButton @click="open" variant="tertiary" class="md:hidden whitespace-nowrap">
-              <template #prefix>
-                <SfIconTune />
-              </template>
-              {{ $t('listSettings') }}
-            </SfButton>
-          </div>
           <section
             v-if="products"
             class="grid grid-cols-1 2xs:grid-cols-2 gap-4 md:gap-6 md:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4 mb-10 md:mb-5"
@@ -122,7 +120,6 @@
         </div>
   </div>
   </div>
-  </NarrowContainer>
 </template>
 
 <script setup lang="ts">
@@ -145,12 +142,12 @@ withDefaults(defineProps<CategoryPageContentProps>(), {
   itemsPerPage: 24,
 });
 
-// const { isOpen, open, close } = useDisclosure();
-const { isTablet, isDesktop } = useBreakpoints();
+const { isOpen: IsRefinebyfilteropen, open, close: IsRefinebyfilterclose } = useDisclosure();
+const { isTablet, isDesktop} = useBreakpoints();
 
 const maxVisiblePages = computed(() => (isDesktop.value ? 5 : 1));
 
-// whenever(isTablet, close);
+whenever(isTablet, IsRefinebyfilterclose);
 
 type SelectOption = {
   label: string;
@@ -176,7 +173,7 @@ const options: SelectOption[] = [
   },
 ];
 
-const { close, toggle, isOpen } = useDisclosure({ initialValue: false });
+const { close: DropdownClose, toggle: RecomendedDropdownToggle, isOpen: IsDropdownOpen } = useDisclosure({ initialValue: false });
 const selectedOption = ref<SelectOption>(options[0]);
 const id = useId();
 const listboxId = `select-dropdown-${id}`;
@@ -187,19 +184,19 @@ const {
   floatingRef,
   style: dropdownStyle,
 } = useDropdown({
-  isOpen,
-  onClose: close,
+  isOpen:IsDropdownOpen,
+  onClose:DropdownClose,
 });
 
 useTrapFocus(floatingRef as Ref<HTMLUListElement>, {
   arrowKeysUpDown: true,
-  activeState: isOpen,
+  activeState: IsDropdownOpen,
   initialFocusContainerFallback: true,
 });
 
 const selectOption = (option: SelectOption) => {
   selectedOption.value = option;
-  close();
+  DropdownClose();
   unrefElement(selectTriggerRef as Ref<HTMLDivElement>)?.focus();
 };
 </script>
