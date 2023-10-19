393f02c3e7ce1509fa03fc4361063d7d609b58fa
ateeqTM <ateeq.ashiq@ki5.co.uk>
2023-10-19 17:30:51 +0500

    uspBar #uspBar mobile desktop completed

---
+++ apps/web/components/TM/Home/USPBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="usp-bar-main bg-[#FAFAFA] mp:bg-white">
+    <div class="usp-bar-inner px-6 py-2 mp:my-2">
+      <Carousel class="w-full" :autoplay='5000' :transition='1000' :breakpoints='uspBarContent.breakpoints'>
+        <Slide v-for="(content, index) in USPBarContent" :key='index' v-if="USPBarContent"
+               @click="navigateToLink(content.url)">
+          <div class="usp-content flex items-center w-full mp:justify-center"
+               :class="{'justify-start': index === 0, 'justify-center': index === 1, 'justify-end': index === USPBarContent.length - 1}">
+            <img :src="content.image" :alt="content.alt" class="tab:w-7 tab:h-7 mp:w-14 mp:h-14"/>
+            <p class="pl-3 text-[#18181B] text-base font-medium tab:text-xs mp:text-base">{{ content.text }}</p>
+          </div>
+        </Slide>
+      </Carousel>
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {reactive, computed} from 'vue';
+import 'vue3-carousel/dist/carousel.css'
+import {Carousel, Navigation, Pagination, Slide} from 'vue3-carousel'
+
+const uspBarContent = reactive({
+  uspData: [
+    {
+      image: '/images/uspBar/delivery.svg',
+      text: 'Free Delivery Over £299',
+      alt: 'Free Delivery',
+      url: '/floor-tiles.html'
+    },
+    {
+      image: '/images/uspBar/Samples.svg',
+      text: 'Totally Free Cut Samples',
+      alt: 'Free Cut Samples',
+      url: '/kitchen-tiles.html'
+    },
+    {
+      image: '/images/uspBar/reviews.svg',
+      text: 'Over 29,000 Positive Reviews',
+      alt: 'Positive Reviews',
+      url: '/floor-tiles.html'
+    }
+  ],
+  breakpoints: {
+    768: {
+      itemsToShow: 3,
+    },
+  },
+})
+
+
+const USPBarContent = computed(() => {
+  return uspBarContent.uspData && uspBarContent.uspData?.length > 0 ? uspBarContent.uspData : null
+})
+
+const navigateToLink = (url: string) => {
+  const router = useRouter();
+  router.push(url)
+}
+
+</script>
--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,11 +1,6 @@
 <template>
-  <div v-for="(component, index) in content" :key="`${component.fields.component}-${index}`">
-    <RenderContent :content="component.fields.content" />
-  </div>
+  <TMHomeUSPBar />
 </template>
 
 <script setup lang="ts">
-const { getContent, data: content } = useContent('home-page');
-
-getContent();
 </script>
